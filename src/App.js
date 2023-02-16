279da45cf65a18c993b520a3d37d34ef3f89b944
Yevhenii Halenko <Re4port@ukr.net>
2023-02-16 21:03:59 +0200

    Add Dockerfile and docker-compose

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello there...
         </p>
+        <p>
+          Build, test, and deploy Docker applications with Kubernetes while learning production-style development workflows
+        </p>
+        <p>Docker and Kubernetes: The Complete Guide</p>
         <a
           className="App-link"
           href="https://reactjs.org"
